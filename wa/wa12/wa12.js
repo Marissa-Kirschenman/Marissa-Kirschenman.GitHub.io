95268a34239ed2ac7e091b5f30e1ae34064bafa6
Marissa-Kirschenman <marissak0521@gmail.com>
2024-07-02 18:26:31 -0600

    wa12

---
+++ wa/wa12/wa12.js
@@ -0,0 +1,26 @@
+async function getJoke() {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json',
+        },
+    });
+
+    const data = await response.json()
+    document.getElementById('joke').innerText = data.joke;
+    getLaughingGif();
+}
+
+async function getLaughingGif() {
+    const apiKey =  'ea1qPFCrhrj00lehkHwiufeHPStYCGiA';
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=laughing&limit=1&offset=${Math.floor(Math.random() * 50)}`);
+    const data = await response.json();
+    const gifUrl = data.data[0]?.images.downsized_medium.url || '';
+    const gifContainer = document.getElementById('gif-container');
+        if (gifUrl) {
+            gifContainer.innerHTML = `<img src="${gifUrl}" alt="GIF" />`;
+        } else {
+            gifContainer.innerHTML = 'No GIF found';
+        }
+        }
+
+        window.onload = getJoke;
